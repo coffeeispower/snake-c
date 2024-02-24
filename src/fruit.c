9d788aef1d92e1651b68e888cf3841fb01b8322d
Tiago Dinis <tiagodinis33@proton.me>
2024-02-24 12:29:35 +0000

    fix: show a red square instead of emoji on windows
    CMD SUUUUCKS

--- src/fruit.c
+++ src/fruit.c
@@ -4,7 +4,12 @@
 
 void fruit_render(struct Fruit* self) {
   move_cursor(self->position.x*2, self->position.y);
+  #ifdef _WIN32
+  set_bg_color_256(196);
+  printf("  ");
+  #else
   printf("🍎");
+  #endif
 }
 bool fruit_snake_collision(struct Fruit* self, struct Snake *snake){
   return snake_is_head(snake, self->position);
